3e703aa22c8acb878459c9965d84d000e0c6f137
Mats Dodd <mats.dodd12@gmail.com>
2025-05-17 15:20:50 -0700

    getting project context

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
 class ProjectContext(BaseModel):
-    user_prompt: str
-    project_mdc_file_contents: str
-    project_package_manager_contents: str
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+    user_prompt: str = Field(description="The user prompt for the project")
+    project_mdc_file_contents: str = Field(description="The contents of the project's mdc file")
+    project_package_manager_contents: str = Field(description="The contents of the project's package manager file eg package.json, pyproject.toml, cargo.toml, etc")
 
 @app.post("/project-context")
 def read_project_context(project_context: ProjectContext) -> str:
